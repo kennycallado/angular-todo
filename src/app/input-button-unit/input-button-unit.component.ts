a8daefbc330c2f8029b0924553a7369c62c3913b
KennyCallado <kennycallado@hotmail.com>
2021-08-03 17:42:09 +0200

    # Commit - 3 - Pequeñas pruebas en input-button-unit

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
+  title: string = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.changeTitle("Hello world");
   }
 
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+    }
+  // La voy a dejar al final
+  ngOnInit(): void {
+  }
 }
