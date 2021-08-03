ff25c4675ee477bc907a55c054d439db0e8576d3
KennyCallado <kennycallado@hotmail.com>
2021-08-03 19:28:34 +0200

    # Commit - 6 - Agregado componente todo-item   - Encargado de mostrar los item   - Recibe input de parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 // El module no es el controler, solo da lógica a angular sobre el componente.
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
