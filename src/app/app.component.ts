955187bb6a1032a0b193051545120109d5a2d7ab
KennyCallado <kennycallado@hotmail.com>
2021-08-04 20:07:58 +0200

    # Commit - 14 - Pequeñas mejoras de la itnerface de usuario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-<div class="container bg-info text-center">
+<div class="container-fluid text-center bg-info" >
   <h2>{{title}}</h2>
-  <hr class="text-secundary"/>
+  <hr />
   <app-list-manager></app-list-manager>
 </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Todo list app';
+  title = 'My todo list';
 }
